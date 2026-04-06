4766e1658188607ecacea9579c761f29af8ade82
Feroz <mohamedferoz2023@gmail.com>
2026-04-06 17:57:38 +0530

    update resume page

--- FrontEnd/src/pages/Resume.jsx
+++ FrontEnd/src/pages/Resume.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Resume = () => {
   return (
     <div>
-    Create resume  with form to fill in details and upload resume file
+    Create resume   fill in details and upload resume file
     </div>
   )
 }
